cdd56487119375da6d264e6358acf313292b24f0
MBOng124 <36434160+MBOng124@users.noreply.github.com>
2019-12-03 19:56:08 +0800

    Added Mathematical Expression Evaluation

--- src/Interpreter.java
+++ src/Interpreter.java
@@ -1,5 +1,13 @@
+import java.math.BigDecimal;
+import java.text.NumberFormat;
+import java.text.ParsePosition;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Scanner;
+
+import com.udojava.evalex.Expression;
+import com.udojava.evalex.Expression.*;
+import static com.udojava.evalex.Expression.e;
 
 public class Interpreter<T> extends javaBaseVisitor<T>{
     HashMap<String, String> var_types;
@@ -61,4 +69,66 @@ public class Interpreter<T> extends javaBaseVisitor<T>{
 
         return visitChildren(ctx);
     }
+
+    @Override
+    public T visitAdditiveExpression(javaParser.AdditiveExpressionContext ctx) {
+        String text = ctx.getText();
+        if(text.contains("+")||text.contains("*") || text.contains("-") || text.contains("/")) {
+            evalMath(ctx);
+        }
+        return visitChildren(ctx);
+    }
+
+    public BigDecimal evalMath(javaParser.AdditiveExpressionContext ctx){
+        BigDecimal result;
+        String text = ctx.getText();
+        StringBuilder chars = new StringBuilder();
+        ArrayList<String> operands, operators;
+        operands = new ArrayList<>();
+        operators = new ArrayList<>();
+        for(int i = 0; i < text.length(); i++){
+            char ins = text.charAt(i);
+            if(ins != '+' && ins != '-' && ins != '*' && ins != '/'){
+                chars.append(ins);
+            }else if(ins == '+' || ins == '-' || ins == '*' || ins == '/') {
+                String operand = chars.toString();
+                operands.add(operand);
+                operators.add(Character.toString(ins));
+                chars = new StringBuilder();
+            }
+        }
+        operands.add(chars.toString());
+        StringBuilder expression = new StringBuilder();
+        int i = 0;
+        int j = 0;
+        while(i < operands.size()){
+            if(isNumeric(operands.get(i))){
+                expression.append(operands.get(i));
+                i++;
+            }else{
+                if(variables.containsKey(operands.get(i))){
+                    expression.append(variables.get(operands.get(i)));
+                    i++;
+                }
+            }
+            if(j < operators.size()){
+                expression.append(operators.get(j));
+                j++;
+            }
+        }
+        String exp = expression.toString();
+        Expression eval = new Expression(exp);
+        eval.setPrecision(2);
+        result = eval.eval();
+        return result;
+    }
+
+    public static boolean isNumeric(String str) {
+        NumberFormat formatter = NumberFormat.getInstance();
+        ParsePosition pos = new ParsePosition(0);
+        formatter.parse(str, pos);
+        return str.length() == pos.getIndex();
+    }
+
+
 }
